11a4eacb3c18a1a85718e112977ac6858bc93176
Johan Hedin <johan.o.hedin@gmail.com>
2021-02-05 20:49:41 +0100

    Octave function to create a LP filter with sinc

---
+++ octave/sincflt.m
@@ -0,0 +1,17 @@
+% Function to create a low pass sinc filter with a window function.
+%
+% fcut must be less than fs/2.
+% N must be a odd number.
+%
+% List of window functions here: https://octave.sourceforge.io/signal/overview.html#Window_Functions
+
+function ret = sincflt(N, fs, fcut, win, varargin)
+fcut_norm = fcut / fs;
+w1 = window(win, N, varargin{:}).';
+N2 = 0.5 * (N -1);
+h1 = sinc((-N2:N2) * 2 * fcut_norm);
+h = h1 .* w1;
+k1 = 1 / sum(h);
+h = h * k1;
+ret = h;
+endfunction
