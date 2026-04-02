4bc1e79c1676ce3157841923c1340b27548adaed
Johan Hedin <johan.o.hedin@gmail.com>
2026-04-02 21:40:50 +0200

    Fix potential gain pumping in LfAGC by smoothing

--- src/agc.hpp
+++ src/agc.hpp
@@ -83,13 +83,15 @@ private:
 
 class LfAGC {
 public:
-    LfAGC(float attack=10.0f, float decay=0.01f, float reference=0.25f, float max_gain=200.0f)
-    : attack_(attack), decay_(decay), reference_(reference), max_gain_(max_gain), gain_(1.0f), active_(false) {}
+    LfAGC(float attack=10.0f, float decay=0.01f, float reference=0.25f, float max_gain=200.0f, float alpha=0.005f)
+    : attack_(attack), decay_(decay), reference_(reference), max_gain_(max_gain), alpha_(alpha), gain_(1.0f),
+      power_(reference_ * reference_), active_(false) {}
 
     void setAttack(float attack) { attack_ = attack; }
     void setDecay(float decay) { decay_ = decay; }
     void setReference(float reference) { reference_ = reference; }
     void setMaxGain(float max_gain) { max_gain_ = max_gain; }
+    void setAlpha(float alpha) { alpha_ = alpha; }
 
     float gain(void) { return gain_; }
     bool active(void) { return active_; }
@@ -99,8 +101,8 @@ public:
     float adjust(float sample) {
         float sample_adjusted = sample * gain_;
 
-        float level = std::abs(sample_adjusted);
-        float error = reference_ - level;
+        power_ += alpha_ * (sample_adjusted * sample_adjusted - power_);
+        float error = reference_ - std::sqrt(power_);
 
         if (error > 0.0f) {
             // Adjusted signal level is under reference. Increase gain
@@ -128,7 +130,9 @@ private:
     float   decay_;
     float   reference_;
     float   max_gain_;
+    float   alpha_;
     float   gain_;
+    float   power_;
     bool    active_;
 };
 
